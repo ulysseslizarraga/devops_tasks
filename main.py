9a1a09febbff5c47df3aa5528da9d794b544321f
Ulysses Lizarraga <ulises.lizarraga@gmail.com>
2020-03-24 14:52:58 -0700

    added pre-retrieving msg

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import os
 import colorama
 import datetime
+from libops import colorstr
 import devops_tasks.devops_adapter as doa
 
 colorama.init()
+clr = colorstr.bcolors()
 format_str = '%m/%d %I:%M %p'
 
 try:
     while True:
+        now = datetime.datetime.now()
+        print(f"\n[{now.strftime(format_str)}]: {clr.WARNING}Retrieving data from DevOps{clr.ENDC}")
         devops_adapter = doa.DevOpsAdapter()
         now = datetime.datetime.now()
-        print(f"\n[{now.strftime(format_str)}]: {devops_adapter.clr.OKGREEN}Retrieved data from DevOps{devops_adapter.clr.ENDC}")
+        print(f"[{now.strftime(format_str)}]: {clr.OKGREEN}Retrieved data from DevOps{clr.ENDC}")
         devops_adapter.print_work_items()
         input()
-
 except KeyboardInterrupt:
     print(f"\nKeyboard interrupt: shutting down")
