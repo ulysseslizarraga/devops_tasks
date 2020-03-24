3b8498ed23aa5283fa3747f30b158dc0a1ddaf46
Ulysses Lizarraga <ulises.lizarraga@gmail.com>
2020-03-23 18:06:53 -0700

    added constant execution; added time stamping

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 import os
 import colorama
+import datetime
 import devops_tasks.devops_adapter as doa
 
 colorama.init()
-doa.DevOpsAdapter()
+format_str = '%m/%d %I:%M %p'
+
+try:
+    while True:
+        devops_adapter = doa.DevOpsAdapter()
+        now = datetime.datetime.now()
+        print(f"\n[{now.strftime(format_str)}]: {devops_adapter.clr.OKGREEN}Retrieved data from DevOps{devops_adapter.clr.ENDC}")
+        devops_adapter.print_work_items()
+        input()
+
+except KeyboardInterrupt:
+    print(f"\nKeyboard interrupt: shutting down")
